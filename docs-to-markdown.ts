e78d4134dceecbbbaf5d917ca41ced5e423cb04f
Chop TRAN <m@chop.dev>
2023-07-29 13:35:12 +0700

    New post developing roadmap for implement local llm

--- docs-to-markdown.ts
+++ docs-to-markdown.ts
@@ -1,21 +1,21 @@
 // deno run --allow-read --allow-net --allow-run --allow-write docs-to-markdown.ts
 
-import * as Colors from 'https://deno.land/std@0.177.0/fmt/colors.ts'
-import yaml from 'https://cdn.skypack.dev/yaml@v2.2.1'
-import {normalize} from 'https://cdn.jsdelivr.net/gh/motss/deno_mod@v0.10.0/normalize_diacritics/mod.ts'
+import * as Colors from 'https://deno.land/std@0.177.0/fmt/colors.ts';
+import yaml from 'https://cdn.skypack.dev/yaml@v2.2.1';
+import {normalize} from 'https://cdn.jsdelivr.net/gh/motss/deno_mod@v0.10.0/normalize_diacritics/mod.ts';
 
 interface PostInfo {
-    title: string
-    link: string
+    title: string;
+    link: string;
 }
 
 interface Posts {
-    released: PostInfo[]
-    drafts: PostInfo[]
+    released: PostInfo[];
+    drafts: PostInfo[];
 }
 
-const fileContent = await Deno.readTextFile('posts.yaml')
-const posts: Posts = yaml.parse(fileContent)
+const fileContent = await Deno.readTextFile('posts.yaml');
+const posts: Posts = yaml.parse(fileContent);
 
 // Download the image link using wget to a pathPrefix
 // NOTE: the directory and path must already exist
@@ -24,60 +24,56 @@ const downloadLinksToPost = async (link: string, outPath: string) => {
         cmd: ['wget', link, `-O${outPath}`],
         stdout: 'piped',
         stderr: 'piped',
-    })
-    const outStr = new TextDecoder().decode(await process.output())
-    const errStr = new TextDecoder().decode(await process.stderrOutput())
-    process.close()
-    console.log(outStr, errStr)
-}
+    });
+    const outStr = new TextDecoder().decode(await process.output());
+    const errStr = new TextDecoder().decode(await process.stderrOutput());
+    process.close();
+    console.log(outStr, errStr);
+};
+
+const processWithDocsToMarkdown = async (link: string) => {
+    const resp = await fetch('https://docs-to-markdown.chop.dev', {
+        method: "POST",
+        headers: {accept: 'application/json'},
+        body: JSON.stringify({url: link}),
+    });
+    return resp.text();
+};
 
 for (const a of posts.drafts) {
-    const process = await Deno.run({
-        cmd: ['docs_to_markdown', a.link],
-        stdout: 'piped',
-        stderr: 'piped',
-    })
+    let outStr = await processWithDocsToMarkdown(a.link);
+    console.log('>>>', outStr);
 
-    const errStr = new TextDecoder().decode(await process.stderrOutput())
-    if (errStr) {
-        console.log(Colors.red('=== Error processing ==='), a.title)
-        console.log(errStr)
-        break
-    }
+    let title: String = await normalize(a.title);
+    title = title.toLowerCase().replace(/[^a-z0-9]/g, '-');
 
-    let outStr = new TextDecoder().decode(await process.output())
-    process.close()
+    console.log(Colors.yellow('Writing:'), Colors.red(title));
+    console.log('Content\n', outStr.slice(0, 500));
 
-    let title: String = await normalize(a.title)
-    title = title.toLowerCase().replace(/[^a-z0-9]/g, '-')
-
-    console.log(Colors.yellow('Writing:'), Colors.red(title))
-    console.log('Content\n', outStr.slice(0, 500))
-
-    const postPath = `./content/posts/${title}`
-    await Deno.mkdir(postPath, {recursive: true})
+    const postPath = `./content/posts/${title}`;
+    await Deno.mkdir(postPath, {recursive: true});
 
     // Download images from googleusercontent
     const downloadImages: string[] = [
         ...(outStr.match(new RegExp(/https.*?googleusercontent.com\/[\/\.?=&\w_-]*/g)) || []),
         ...(outStr.match(new RegExp(/https.*?ytimg.com\/[\/\.?=&\w_-]*/g)) || []),
         ...(outStr.match(new RegExp(/https.*?ibb.co\/[\/\.?=&\w_-]*/g)) || []),
-    ]
+    ];
     const downloads = Object.entries(downloadImages).map(([index, link]) => ({
         link,
         path: `${postPath}/${title}-${index}.jpg`,
         articleReplace: `/posts/${title}/${title}-${index}.jpg`,
-    }))
+    }));
     for (const image of downloads) {
-        await downloadLinksToPost(image.link, image.path)
-        outStr = outStr.replaceAll(image.link, image.articleReplace)
+        await downloadLinksToPost(image.link, image.path);
+        outStr = outStr.replaceAll(image.link, image.articleReplace);
     }
 
     outStr = outStr
         // Cleanup the unexpected characters from Docs
         .replaceAll('”', '"')
         .replaceAll('“', '"')
-        .replaceAll(' ', ' ') // These are actually 2 diferent character for space
+        .replaceAll(' ', ' '); // These are actually 2 diferent character for space
 
-    await Deno.writeTextFile(`./content/posts/${title}/${title}.md`, outStr)
+    await Deno.writeTextFile(`./content/posts/${title}/${title}.md`, outStr);
 }
