b80ca0333beeabaf1d538848bd498154c288472e
Chop TRAN <m@chop.dev>
2024-09-26 08:52:01 +0700

    Fix build revealjs script

--- scripts/build-revealjs.sh
+++ scripts/build-revealjs.sh
@@ -6,8 +6,10 @@ wget https://github.com/hakimel/reveal.js/archive/master.zip -O revealjs.zip
 
 unzip revealjs.zip
 
+mkdir -p $DEST
 cp -r reveal.js-master/dist $DEST
 cp -r reveal.js-master/plugin $DEST
+ls $DEST
 
 rm -r reveal.js-master
 rm revealjs.zip
